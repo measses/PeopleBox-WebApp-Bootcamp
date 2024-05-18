7a2f9a0a25465165b77d2af5506018502489f1bf
Mert Araz <mertoreyiz1907@gmail.com>
2024-05-19 02:07:12 +0300

    Converted to foreach structure

--- blog-app/index.php
+++ blog-app/index.php
@@ -55,117 +55,44 @@ $filmler["0"] = $yeni_film;
         <div class="row">
             <div class="col-3">
                 <ul class="list-group">
-                    <li class="list-group-item"><?php echo $kategori[0] ?></li>
-                    <li class="list-group-item"><?php echo $kategori[1] ?></li>
-                    <li class="list-group-item"><?php echo $kategori[2] ?></li>
-                    <li class="list-group-item"><?php echo $kategori[3] ?></li>
-                    <li class="list-group-item"><?php echo $kategori[4] ?></li>
+                   <?php
+                    foreach ($kategori as $kategoriler) {
+                        echo "<li class=\"list-group-item\">$kategoriler</li>";
+                    }
+                   ?>
                 </ul>
             </div>
-            <div class="col-9">
-                <h3 class="text-center"><?php echo $film_baslik ?></h3>
-                <div class="card mb-3">
-                    <div class="row">
-
-                        <div class="col-3">
-                            <?php echo
-                                "<img class=\"img-fluid\" src=\"img/{$filmler["1"]["resim"]}\">" ?>
-                        </div>
-                        <div class="col-9">
-
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $filmler["1"]["baslik"] ?></h5>
-                                <p class="card-text">
-                                    <?php echo ucfirst(strtolower(substr($filmler[1]["aciklama"], 0, 50))) . '...'; ?>
-                                </p>
-                                <div>
-                                    <span class="badge bg-success">Yapım Tarihi: 03.12.2021</span>
-                                    <span class="badge bg-success"><?php echo $filmler["1"]["yorumSayisi"] ?></span>
-                                    <span class="badge bg-success"><?php echo $filmler["1"]["begeniSayisi"] ?></span>
-                                    <span class="badge bg-success"><?php echo $filmler["1"]["vizyon"] ?></span>
-
-                                </div>
-                                <div class="mt-3">
-                                    <p>
-                                        <a target="_blank" href="<?php echo $filmler[1]["url"]; ?>"
-                                            class="btn btn-outline-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                                            Film Link
-                                        </a>
-
-                                    </p>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3">
-                    <div class="row">
-                        <div class="col-3">
-                            <?php echo
-                                "<img class=\"img-fluid\" src=\"img/{$filmler["2"]["resim"]}\">" ?>
-                        </div>
-                        <div class="col-9">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $filmler["2"]["baslik"] ?></h5>
-                                <p class="card-text">
-                                    <?php echo ucfirst(strtolower(substr($filmler[2]["aciklama"], 0, 50))) . '...'; ?>
-
-
-                                </p>
-                                <div>
-                                    <span class="badge bg-success">Yapım Tarihi: 03.12.2021</span>
-                                    <span class="badge bg-success"><?php echo $filmler["2"]["yorumSayisi"] ?></span>
-                                    <span class="badge bg-success"><?php echo $filmler["2"]["begeniSayisi"] ?></span>
-                                    <span class="badge bg-success"><?php echo $filmler["2"]["vizyon"] ?></span>
-                                </div>
-                                <div class="mt-3">
-                                    <p>
-                                        <a target="_blank" href="<?php echo $filmler[2]["url"]; ?>"
-                                            class="btn btn-outline-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                                            Film Link
-                                        </a>
-
-                                    </p>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3">
-                    <div class="row">
-                        <div class="col-3">
-                            <?php echo
-                                "<img class=\"img-fluid\" src=\"img/{$filmler["0"]["resim"]}\">" ?>
-                        </div>
-                        <div class="col-9">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $filmler["0"]["baslik"] ?></h5>
-                                <p class="card-text">
-                                    <?php echo ucfirst(strtolower(substr($filmler[0]["aciklama"], 0, 50))) . '...'; ?>
-
-                                </p>
-                                <div>
-                                    <span class="badge bg-success">Yapım Tarihi: 03.12.2021</span>
-                                    <span class="badge bg-success"><?php echo $filmler["0"]["yorumSayisi"] ?></span>
-                                    <span class="badge bg-success"><?php echo $filmler["0"]["begeniSayisi"] ?></span>
-                                    <span class="badge bg-success"><?php echo $filmler["0"]["vizyon"] ?></span>
-                                </div>
-                                <div class="mt-3">
-                                    <p>
-                                        <a target="_blank" href="<?php echo $filmler[0]["url"]; ?>"
-                                            class="btn btn-outline-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                                            Film Link
-                                        </a>
-                                    </p>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <div class="col-9">
+          <h3 class="text-center"><?php echo $film_baslik ?></h3>
+      
+          <?php foreach ($filmler as $film): ?>
+          <div class="card mb-3">
+              <div class="row">
+      
+                  <div class="col-3">
+                      <img class="img-fluid" src="img/<?php echo $film['resim']; ?>">
+                  </div>
+                  <div class="col-9">
+                      <div class="card-body">
+                          <h5 class="card-title"><?php echo $film['baslik']; ?></h5>
+                          <p class="card-text">
+                              <?php echo ucfirst(strtolower(substr($film['aciklama'], 0, 50))) . '...'; ?>
+                          </p>
+                          <div>
+                              <span class="badge bg-success">Yapım Tarihi: 03.12.2021</span>
+                              <span class="badge bg-success"><?php echo $film['yorumSayisi']; ?></span>
+                              <span class="badge bg-success"><?php echo $film['begeniSayisi']; ?></span>
+                              <span class="badge bg-success"><?php echo $film['vizyon']; ?></span>
+                          </div>
+                      </div>
+                  </div>
+      
+              </div>
+          </div>
+          <?php endforeach; ?>
+      
+      </div>
+      
         </div>
     </div>
 </body>
