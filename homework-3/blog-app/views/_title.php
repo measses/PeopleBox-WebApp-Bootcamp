9bc1ebd778374aac339339db4336c3348212279d
Mert Araz <mertoreyiz1907@gmail.com>
2024-05-23 14:35:39 +0300

    homework-3 done

--- homework-3/blog-app/views/_title.php
+++ homework-3/blog-app/views/_title.php
@@ -1,9 +1,25 @@
 <?php
-    $ozet = count(getData()["categories"]).' kategoride '.count(getData()["movies"]).'  film listelenmiştir';
-    const baslik = "Popüler Filmler";
+$data = getData();
+$activeMovies = array_filter($data["movies"], function($movie) {
+    return $movie["is-active"];
+});
+$ozet = count($data["categories"]) . ' kategoride ' . count($activeMovies) . ' film listelenmiştir';
+const BASLIK = "Popüler Filmler";
 ?>
-    
-<h1 class="mb-4"><?php echo baslik?></h1>
+
+<h1 class="mb-4"><?php echo BASLIK; ?></h1>
 <p class="text-muted">
-    <?php echo $ozet?>
+    <?php if (isset($data["categories"])): ?>
+        <?php echo $ozet; ?>
+    <?php endif; ?>
 </p>
+
+<?php if (empty($activeMovies)): ?>
+    <div class="alert alert-warning" role="alert">
+        Henüz film eklenmemiştir.
+    </div>
+<?php else: ?>
+    <div class="alert alert-success" role="alert">
+        <?php echo count($activeMovies); ?> aktif film bulundu.
+    </div>
+<?php endif; ?>
